ca6a22cb023a32b0c5f5f5a537e0af03846ca0fc
Wither <86643519+ItsDaWither@users.noreply.github.com>
2022-01-03 23:24:20 -0800

    init commit

---
+++ NotificationSpammer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace SpamMoment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+                new ToastContentBuilder().AddText("\u200F").Show();
+            }
+        }
+    }
+}
